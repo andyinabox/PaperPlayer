9d0e2ff6a752c0ffe05a5a822c052c0768bc2d41
Andy Dayton <amdayton@gmail.com>
2013-06-15 11:55:13 -0400

    First Commit

---
+++ src/PaperPlayer.cpp
@@ -0,0 +1,25 @@
+//
+//  PaperPlayer.cpp
+//  PaperPlayer
+//
+//  Created by Andrew Dayton on 6/15/13.
+//
+//
+
+#include "PaperPlayer.h"
+
+class PaperPlayer {
+public:
+    // why is this necessary? just copying like a sheep for now
+    PaperPlayer();
+    ~PaperPlayer();
+    
+    void setup();
+    int update();
+    void draw();
+    
+protected:
+    
+private:
+    
+};
---
+++ src/PaperPlayer.h
@@ -0,0 +1,19 @@
+//
+//  PaperPlayer.h
+//  PaperPlayer
+//
+//  Created by Andrew Dayton on 6/15/13.
+//
+//
+
+#ifndef __PaperPlayer__PaperPlayer__
+#define __PaperPlayer__PaperPlayer__
+
+#include <iostream>
+#include "ofxOpenCv.h"
+#include "ofxARToolkitPlus.h"
+#include "ofMain.h"
+
+
+
+#endif /* defined(__PaperPlayer__PaperPlayer__) */
